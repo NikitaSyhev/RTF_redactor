6fe7e501dd206ecf5c01a75809c057283c89bef3
Nikita Sychev <nikitasychev@inbox.ru>
2023-08-19 19:09:54 +0500

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace RTF_redactor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openBtn = new System.Windows.Forms.Button();
+            this.saveBtn = new System.Windows.Forms.Button();
+            this.TextBox = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // openBtn
+            // 
+            this.openBtn.Location = new System.Drawing.Point(12, 12);
+            this.openBtn.Name = "openBtn";
+            this.openBtn.Size = new System.Drawing.Size(122, 35);
+            this.openBtn.TabIndex = 0;
+            this.openBtn.Text = "Open";
+            this.openBtn.UseVisualStyleBackColor = true;
+            this.openBtn.MouseClick += new System.Windows.Forms.MouseEventHandler(this.openBtn_MouseClick);
+            // 
+            // saveBtn
+            // 
+            this.saveBtn.Location = new System.Drawing.Point(12, 71);
+            this.saveBtn.Name = "saveBtn";
+            this.saveBtn.Size = new System.Drawing.Size(122, 35);
+            this.saveBtn.TabIndex = 1;
+            this.saveBtn.Text = "Save";
+            this.saveBtn.UseVisualStyleBackColor = true;
+            // 
+            // TextBox
+            // 
+            this.TextBox.Location = new System.Drawing.Point(372, 20);
+            this.TextBox.Name = "TextBox";
+            this.TextBox.Size = new System.Drawing.Size(416, 418);
+            this.TextBox.TabIndex = 2;
+            this.TextBox.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.TextBox);
+            this.Controls.Add(this.saveBtn);
+            this.Controls.Add(this.openBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.FormTheme);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button openBtn;
+        private System.Windows.Forms.Button saveBtn;
+        private System.Windows.Forms.RichTextBox TextBox;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RTF_redactor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+           
+
+        }
+
+        private void FormTheme(object sender, EventArgs e)
+        {
+            DateTime currentTime = DateTime.Now;
+            bool isDay = currentTime.Hour >= 6 &  currentTime.Hour <= 18;
+            if (isDay)
+            {
+                BackColor = Color.White;
+                ForeColor = Color.Black;
+            }
+            else
+            {
+                BackColor = Color.Black;
+                ForeColor = Color.White;
+            }
+        }
+
+        private void openBtn_MouseClick(object sender, MouseEventArgs e)
+        {
+            string result = OpenFile();
+            if (result != "none")
+            {
+                TextBox.LoadFile(result);
+            }
+        }
+
+        private string OpenFile()
+        {
+            using (OpenFileDialog of = new OpenFileDialog())
+            {
+                of.Filter = "RTF files (*.rtf)|*.rtf";
+                of.InitialDirectory = Environment.CurrentDirectory;
+
+                if (of.ShowDialog() == DialogResult.OK)
+                {
+                    return of.FileName;
+                }
+            }
+            return "none";
+        }
+    }
+ 
+}
